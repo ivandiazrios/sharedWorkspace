c115a2df7cf0d4ced22cfc6167e8682448e7b09e
Ivan Diaz Rios <ivan.diaz-rios@shazam.com>
2017-07-25 18:25:56 +0100

    Cleanup unused.

--- Utils.py
+++ Utils.py
@@ -1,47 +1,8 @@
 import random
 import string
 
-def tabsToSpaces(string, tab_space=4):
-    return string.replace('\t', tab_space*' ')
-
-def appendToEndOfString(string, string_to_append):
-    endInNewline = False
-    if string[-1] == '\n':
-        endInNewline = True
-        string = string[:-1]
-
-    lines = tabsToSpaces(string).split('\n')
-    max_line_length = max([len(line) for line in lines])
-    pad_formatter = "{:%s}" % (max_line_length + 1)
-    output = '\n'.join([pad_formatter.format(line) + string_to_append for line in lines])
-    if endInNewline:
-        output += '\n'
-    return output
-
 def uuid(bits=24):
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(bits))
-
-class Stack:
-    def __init__(self):
-        self.__storage = []
-
-    def isEmpty(self):
-        return len(self.__storage) == 0
-
-    def push(self,p):
-        self.__storage.append(p)
-
-    def pop(self):
-        return self.__storage.pop()
-
-    def peek(self):
-        return self.__storage[-1]
-
-    def __repr__(self):
-        return self.__storage.__repr__()
-
-    def __iter__(self):
-        return iter(self.__storage)
 
 def lazy_property(fn):
     '''Decorator that makes a property lazy-evaluated.

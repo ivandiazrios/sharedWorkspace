89fadde9f64ce660bc996b967ba9d7fae8737e6d
Ivan Diaz Rios <ivan.diaz-rios@shazam.com>
2017-07-27 18:30:57 +0100

    Command line argparse.

--- Shared.py
+++ Shared.py
@@ -8,12 +8,12 @@ import argparse
 if __name__ == "__main__":
     # Command line arguments #####################
     commandLineParser = argparse.ArgumentParser()
-    commandLineParser.add_argument("targetProjectPath", help="Path to first project")
-    commandLineParser.add_argument('-s', '--shared', nargs='+', type=str)
-    commandLineParser.add_argument('-s', '')
+    commandLineParser.add_argument("targetProjectDirectory", help="Target project directory")
+    commandLineParser.add_argument('-s', '--shared', nargs='+', type=str, help="Shared project directories", required=True)
+    commandLineParser.add_argument('-t', '--target', help="Specific target for shared workspace... defaults to the target with the same name as the project if any exists")
     commandLineArgs = commandLineParser.parse_args()
-    targetProjectPath = commandLineArgs.targetProjectPath
-    sharedProjectPaths = commandLineArgs.shared
+    targetProjectPath = commandLineArgs.targetProjectDirectory
+    sharedProjectPaths = commandLineArgs.shared if commandLineArgs.shared else []
     targetToShareInto = commandLineArgs.target
     ##############################################
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     sharedProjectPaths = [XcodeProject(sharedProjectPath) for sharedProjectPath in sharedProjectPaths]
 
     sharedWorkspace = SharedWorkspace(targetProject, *sharedProjectPaths)
-    additionDict, subtractDict = sharedWorkspace.share()
+    additionDict, subtractDict = sharedWorkspace.share(targetToShareInto)
 
     output = PlistModifier(targetProject.plistFilePath).process(additionDict, subtractDict)
     with open(targetProject.plistFilePath, 'w') as file:

996e134547879736242840d9fe9666f8a392692d
Ivan Diaz Rios <ivan.diaz-rios@shazam.com>
2017-07-27 16:19:29 +0100

    Found bug.

--- SharedWorkspace.py
+++ SharedWorkspace.py
@@ -47,13 +47,6 @@ class SharedWorkspace(object):
             raise MissingTargetException("Cannot find target to merge into")
         return target
 
-    def resetShared(self):
-        self.containerPortal = uuid(24)
-        self.libFileId = uuid(24)
-        self.productGroupId = uuid(24)
-        self.referenceProxyTargetIds = defaultdict(lambda: uuid(24))
-        self.containerItemProxyTargetIds = defaultdict(lambda: uuid(24))
-
     def getKeyOfFirstValueMatchingKeyValue(self, l, *keyValues):
         dictContainsKeyValues = lambda dict: all((dict.get(k, None) == v for k, v in keyValues))
         return next((k for k,v in l if dictContainsKeyValues(v)), None)
@@ -172,7 +165,7 @@ class SharedWorkspace(object):
         projectId = self.targetProject.plistObj[ROOT_OBJECTS_KEY]
         projectReferences = self.targetProject.plistObj[OBJECTS_KEY].get(projectId, {}).get(PROJECT_REFERENCES_KEY, [])
 
-        projectReference = next((projectReference for projectReference in projectReferences if projectReference.get(PRODUCT_REFERENCE_KEY, None) == containerPortal), None)
+        projectReference = next((projectReference for projectReference in projectReferences if projectReference.get(PROJECT_REFERENCE_KEY, None) == containerPortal), None)
         if projectReference:
             return projectReference[PRODUCT_GROUP_KEY]
 
@@ -180,7 +173,7 @@ class SharedWorkspace(object):
 
         projectReference = {
             PRODUCT_GROUP_KEY: productGroupId,
-            PRODUCT_REFERENCE_KEY: containerPortal
+            PROJECT_REFERENCE_KEY: containerPortal
         }
 
         self.additionDict.setdefault(OBJECTS_KEY, {}).setdefault(projectId, {}).setdefault(PROJECT_REFERENCES_KEY, []).insert(0, projectReference)
@@ -198,9 +191,8 @@ class SharedWorkspace(object):
 
     def frameworkNameForTarget(self, target):
         targetName = target.name
-        if target.endswith("Lib"):
-            return targetName[:-3] # Remove lib
-
+        if targetName.endswith("Lib"):
+            return targetName[:-3] + ".framework"
 
     def replaceDependFrameworkWithSharedWorkspaceLib(self, targetTarget, sharedTarget, referenceProxyTargetIdForProductReference):
         targetFrameworkPhase = self.targetFrameworkPhaseForTarget(targetTarget)
@@ -208,15 +200,16 @@ class SharedWorkspace(object):
 
         shouldAddLibBuildFile = True
         referenceIdForTargetTarget = referenceProxyTargetIdForProductReference[sharedTarget.productReference]
+        frameworkNameToRemove = self.frameworkNameForTarget(sharedTarget)
 
         # Iterate through files until we find our framework one... then remove it.
         for file in targetFrameworkPhaseFiles:
             fileRefId = self.targetProject.plistObj[OBJECTS_KEY][file][FILE_REFERENCE_KEY]
             fileName = self.targetProject.plistObj[OBJECTS_KEY].get(fileRefId, {}).get(NAME_KEY, "")
-            if fileName.startswith(sharedTarget.name):
+            if fileName == frameworkNameToRemove:
                 self.subtractionDict.setdefault(OBJECTS_KEY, {}).setdefault(targetFrameworkPhase, {}).setdefault(FILES_KEY, []).insert(0, file)
                 self.subtractionDict[OBJECTS_KEY][file] = None
-            if fileRefId == referenceIdForTargetTarget:
+            elif fileRefId == referenceIdForTargetTarget:
                 shouldAddLibBuildFile = False
 
         if shouldAddLibBuildFile:
